5a5eccacb2a876d95206b8d97a6fc84fb19715bd
David Perelman <dperelman3@gmail.com>
2024-07-31 20:27:46 +0300

    components - Links.jsx - add session buttons

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
 
@@ -9,11 +8,25 @@ const Links = () => {
     { title: "Contact", path: "/contact" },
     { title: "Blog", path: "/blog" },
   ];
+
+  const session = true;
+  const isAdmin = true;
+
   return (
     <div className={styles.links}>
       {links.map((link) => (
         <NavLink item={link} key={link.title}></NavLink>
       ))}
+      {session ? (
+        <>
+          {isAdmin && (
+            <NavLink item={{ title: "Admin", path: "/admin" }}></NavLink>
+          )}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: "Login", path: "/login" }}></NavLink>
+      )}
     </div>
   );
 };
