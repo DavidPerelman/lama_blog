1ec2029fca3ce87a965b14763a7169e4868ce923
David Perelman <dperelman3@gmail.com>
2024-07-31 20:17:26 +0300

    components - NavLink.jsx - styling active link

--- src/components/navbar/links/navLink/NavLink.jsx
+++ src/components/navbar/links/navLink/NavLink.jsx
@@ -1,8 +1,22 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./navLink.module.css";
+import { usePathname } from "next/navigation";
 
 const NavLink = ({ item }) => {
-  return <Link href={item.path}>{item.title}</Link>;
+  const pathName = usePathname();
+
+  return (
+    <Link
+      href={item.path}
+      className={`${styles.container} ${
+        pathName === item.path && styles.active
+      }`}
+    >
+      {item.title}
+    </Link>
+  );
 };
 
 export default NavLink;
