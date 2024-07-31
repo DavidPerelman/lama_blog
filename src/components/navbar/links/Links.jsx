24c7310b47d18daae62e928cddf9bc8b85fba5a5
David Perelman <dperelman3@gmail.com>
2024-07-31 20:12:22 +0300

    components - Navbar.jsx - styling Links

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import styles from "./links.module.css";
+import NavLink from "./navLink/NavLink";
 
 const Links = () => {
   const links = [
@@ -8,11 +10,9 @@ const Links = () => {
     { title: "Blog", path: "/blog" },
   ];
   return (
-    <div>
+    <div className={styles.links}>
       {links.map((link) => (
-        <Link href={link.path} key={link.title}>
-          {link.title}
-        </Link>
+        <NavLink item={link} key={link.title}></NavLink>
       ))}
     </div>
   );
