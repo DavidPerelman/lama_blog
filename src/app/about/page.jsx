cfd5bdb3d58b8e7c852f27e5896c83ae8f2951ce
David Perelman <dperelman3@gmail.com>
2024-08-05 19:44:27 +0300

    app - about - page.jsx - external images

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/27244368/pexels-photo-27244368/free-photo-of-lake-viti-on-iceland.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
