6817687d04a4c7e2fa11eef3520c6234a1564b92
David Perelman <dperelman3@gmail.com>
2024-08-05 20:14:38 +0300

    app - page.jsx - design home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styles from "./home.module.css";
+import Image from "next/image";
 
 const Home = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+          molestiae voluptatibus ex, ipsa quibusdam laudantium rerum repudiandae
+          repellendus vel doloremque ad molestias, deserunt suscipit recusandae
+          excepturi fuga? Dicta, qui distinctio?
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brand} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
