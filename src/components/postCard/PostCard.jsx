cce3a606eca06d6228ea8b985aa19c2bd25a7fff
David Perelman <dperelman3@gmail.com>
2024-08-06 13:01:01 +0300

    app - blog - page.jsx - design blog page

---
+++ src/components/postCard/PostCard.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import styles from "./postCard.module.css";
+import Link from "next/link";
+
+const PostCard = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/12749678/pexels-photo-12749678.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            className={styles.img}
+          />
+        </div>
+        <span className={styles.date}>01.01.2024</span>
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>Title</h1>
+        <p className={styles.desc}>Desc</p>
+        <Link href="">READ MORE</Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
