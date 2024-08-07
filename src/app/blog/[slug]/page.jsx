c5ffed2767c3aa5dc870001ba10fda1ab6f6defb
David Perelman <dperelman3@gmail.com>
2024-08-07 19:31:53 +0300

    app - blog - page.jsx - design blog page

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,45 @@
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+
 const SinglePostPage = () => {
-  return <div>SinglePostPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/12749678/pexels-photo-12749678.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            className={styles.avatar}
+            src="/avatar.jpg"
+            alt=""
+            width={50}
+            height={50}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>David Perelman</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis
+          perferendis error itaque ab officiis animi, ea autem quaerat nihil
+          perspiciatis laudantium consequatur nisi enim, explicabo consectetur
+          quis velit magnam incidunt!
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
