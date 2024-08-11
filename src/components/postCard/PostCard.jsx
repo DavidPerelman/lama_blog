2300f098feab0b06bc82f1b4ba12b2cab8637d58
David Perelman <dperelman3@gmail.com>
2024-08-12 02:57:46 +0300

    app - blog - page.jsx - getData

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./postCard.module.css";
 import Link from "next/link";
 
-const PostCard = () => {
+const PostCard = ({ post }) => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -17,8 +17,8 @@ const PostCard = () => {
         <span className={styles.date}>01.01.2024</span>
       </div>
       <div className={styles.bottom}>
-        <h1 className={styles.title}>Title</h1>
-        <p className={styles.desc}>Desc</p>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>{post.body}</p>
         <Link href="">READ MORE</Link>
       </div>
     </div>
