a9b8800e9dad1124891dd95ac0372aa058d3739e
David Perelman <dperelman3@gmail.com>
2024-08-12 21:54:05 +0300

    app - blog - [slug] - page.jsx - finish

--- src/lib/data.js
+++ src/lib/data.js
@@ -28,7 +28,7 @@ export const getPosts = async () => {
 export const getPost = async (slug) => {
   try {
     connectToDb();
-    const post = await Post.find({ slug });
+    const post = await Post.findOne({ slug });
     return post;
   } catch (error) {
     console.log(error);
--- src/lib/models.js
+++ src/lib/models.js
@@ -16,8 +16,8 @@ const postSchema = new mongoose.Schema(
     title: { type: String, required: true },
     desc: { type: String, required: true },
     img: { type: String },
-    userId: { type: Boolean, required: true },
-    slug: { type: Boolean, required: true, unique: true },
+    userId: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
   },
   { timestamps: true }
 );
