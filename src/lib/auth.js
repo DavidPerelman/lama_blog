3ccf2356dbc80c5b76c38cbea49c288cec631b27
David Perelman <dperelman3@gmail.com>
2024-08-14 21:53:00 +0300

    add new user to database

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { connectToDb } from "./connectToDb";
+import { User } from "./models";
 export const {
   handlers: { GET, POST },
   auth,
@@ -13,8 +15,27 @@ export const {
     }),
   ],
   callbacks: {
-    async signIn(user, account, profile) {
+    async signIn({ user, account, profile }) {
       console.log(user, account, profile);
+      if (account.provider === "github") {
+        connectToDb();
+        try {
+          const user = await User.findOne({ email: profile.email });
+
+          if (!user) {
+            const newUser = new User({
+              username: profile.login,
+              email: profile.email,
+              image: profile.avatar_url,
+            });
+
+            await newUser.save();
+          }
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
+      }
       return true;
     },
   },
