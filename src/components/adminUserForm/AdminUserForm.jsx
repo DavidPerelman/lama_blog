b16b5d625e23fd788a2a35cd24b557aaa7dd7e52
David Perelman <dperelman3@gmail.com>
2024-08-19 15:35:32 +0300

    setup AdminUserForm page

--- src/components/adminUserForm/AdminUserForm.jsx
+++ src/components/adminUserForm/AdminUserForm.jsx
@@ -1,7 +1,39 @@
+"use client";
+
+import { addUser } from "@/lib/action";
+import { useFormState } from "react-dom";
 import styles from "./adminUserForm.module.css";
+import { useEffect, useState } from "react";
 
 const AdminUserForm = () => {
-  return <div>AdminUserForm</div>;
+  const [isClient, setIsClient] = useState(false);
+  const [state, formAction] = useFormState(addUser, undefined);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <>
+      {isClient ? (
+        <form action={formAction} className={styles.container}>
+          <h1>Add New User</h1>
+          <input type="text" placeholder="Username" name="username" />
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <select name="isAdmin">
+            <option value="false">Is Admin?</option>
+            <option value="false">No</option>
+            <option value="true">Yes</option>
+          </select>
+          <button>Add</button>
+          {state && state.error}
+        </form>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 };
 
 export default AdminUserForm;
